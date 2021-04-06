9696becb4111858892c362f612673bd4b4a5fae0
audrius-savickas <umarasss@gmail.com>
2021-04-07 00:19:27 +0300

    basic html, css and testing js

--- scripts.js
+++ scripts.js
@@ -1,10 +1,52 @@
+let form = document.getElementById("form");
+let submit = document.getElementById("submit")
 
-element = document.querySelector("#title");
-console.log(element);
-let i = 0;
+form.addEventListener("submit", onSubmit);
 
-setInterval(setSecond, 1000);
+function onSubmit(event) {
+    event.preventDefault();
 
-function setSecond () {
-    element.innerText = `Seconds ${i++}`
+    let name = document.getElementById("nameSubm");
+    let price = document.getElementById("priceSubm");
+    let heat = document.getElementById("heatSubm");
+    let toppings = document.querySelectorAll("input[type='checkbox']");
+    let photos = document.querySelectorAll("input[type='radio']");
+
+    validateData(name, price, heat, toppings, photos);
+}
+
+function validateData (name, price, heat, toppings, photos) {
+    let isValid = true;
+    let errorList = document.getElementById("errorList");
+    // Remove all error list items
+    while (errorList.firstChild) {
+        errorList.removeChild(errorList.firstChild);
+    }
+    if (name.value.length < 1) { 
+        let node = document.createElement("li");
+        node.textContent = "Name required";
+        errorList.appendChild(node);
+        isValid = false;
+    }
+    if (price.value <= 0) {
+        let node = document.createElement("li");
+        node.textContent = "Value has to be positive";
+        errorList.appendChild(node);
+        isValid = false;
+    }
+    let checked = 0;
+    toppings.forEach(function(topping) {
+        if (topping.checked) {
+            checked++;
+        }
+    });
+    if (checked < 2) {
+        let node = document.createElement("li");
+        node.textContent = "Two or more toppings have to be added";
+        errorList.appendChild(node);
+        isValid = false;
+    }
+    if (isValid) {
+
+    }
 }
